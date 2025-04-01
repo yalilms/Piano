f33a20174be9f20a41314dc35d30dd3a26fa163d
yalilms <yalil.ms72@gmail.com>
2025-04-01 13:46:43 +0200

    cambio

---
+++ src/jd/piano/teclas/Piano.java
@@ -0,0 +1,24 @@
+package jd.piano.teclas;
+
+import javax.swing.text.Position;
+import java.awt.*;
+
+public abstract class Piano {
+    private int teclaInicial;
+    private int teclaFinal;
+    private Position posicion;
+    private Graphics graphics;
+
+    public Piano(){
+
+    }
+    // public abstract Tecla getTecla(int canal, int nota);
+
+    public int getTeclaInicial() {
+        return this.teclaInicial;
+    }
+
+    public int getTeclaFinal() {
+        return this.teclaFinal;
+    }
+}
---
+++ src/jd/piano/teclas/TeclaFactory.java
@@ -0,0 +1,8 @@
+package jd.piano.teclas;
+
+public class TeclaFactory {
+    private TeclaFactory(){
+
+    }
+    // public static  Tecla crearTecla(int nota)
+}

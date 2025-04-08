d8b78bc0b0f85c3c46d488bfe30d1a74b60d39eb
Juansan404 <juansanchezantunezo@gmail.com>
2025-04-08 11:00:19 +0200

    Añadidas siguientes clases: Tecla.java, TeclaBlanca.java, TeclaBlanca1.java, TeclaBlanca2.java, TeclaBlanca3.java y TeclaNegra.java (todas las clases sin programar aun)

--- src/jd/piano/teclas/Tecla.java
+++ src/jd/piano/teclas/Tecla.java
@@ -9,11 +9,59 @@ public abstract class Tecla implements Pulsable{
     private Color colorPulsada;
     private Graphics graphics;
 
-    Tecla(int n) {}
+    Tecla(int n) {
+        this.nota = n;
 
+        this.posicion = null;
+        this.pulsada = false;
+        this.colorPulsada = null;
+        this.graphics = null;
+    }
 
     public int getNumeroNota() {
-        throw new UnsupportedOperationException();
+        return this.nota;
+    }
+
+    @Override
+    public void setPosicion(int x, int y) {
+        this.posicion = new Point(x, y);
+    }
+
+    @Override
+    public void pulsar() {
+        this.pulsada = true;
+    }
+
+    @Override
+    public void soltar() {
+        this.pulsada = false;
+    }
+
+    @Override
+    public void dibujar() {
+        if (this.posicion == null || this.graphics == null) {
+            throw new IllegalStateException("Hay que llamar a setPosicion y setGraphics "+
+                    "antes de llamar al metodo dibujar");
+        }
+        this.graphics.setColor(this.getColor());
+        this.graphics.fillPolygon(this.getVerticesX(), this.getVerticesY(), 4);
+        this.graphics.setColor(Color.BLACK);
+        this.graphics.drawPolygon(this.getVerticesX(), this.getVerticesY(), 4);
+    }
+
+    @Override
+    public void setColorPulsado(Color c) {
+        this.colorPulsada = c;
+    }
+
+    @Override
+    public boolean estaPulsado() {
+        return this.pulsada;
+    }
+
+    @Override
+    public Color getColorPulsado() {
+        return this.colorPulsada;
     }
 
     protected abstract int[] getVerticesX();

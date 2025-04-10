7a84085896b35df2a21267c1706e41e37c3a22e7
Juansan404 <juansanchezantunezo@gmail.com>
2025-04-10 20:04:30 +0200

    Terminado TeclaBlanca1.java, FALTAN LOS TESTS

--- src/jd/piano/teclas/TeclaBlanca1.java
+++ src/jd/piano/teclas/TeclaBlanca1.java
@@ -11,11 +11,15 @@ public class TeclaBlanca1 extends TeclaBlanca {
 
     @Override
     protected int[] getVerticesX() {
-        return new int[0];
+        int x = this.posicion.x;
+        final int N = TeclaNegra.ANCHURA;
+        final int B = TeclaBlanca.ANCHURA;
+        return new int[] {x, x, x+B, x+B, x+B-N/2, x+B-N/2};
     }
 
     @Override
     protected int[] getVerticesY() {
-        return new int[0];
+        int y = this.posicion.y;
+        return new int[] {y, y+TeclaBlanca.ALTURA, y+TeclaBlanca.ALTURA, y+TeclaNegra.ALTURA, y+TeclaNegra.ALTURA, y};
     }
 }

a76eb55abfb6f0e00f0ecf1c3c83289a37bfd54a
Alberto <aperezoncina@gmail.com>
2025-04-10 16:41:51 +0200

    ReproductorMidi --> PianoMidi no existe he puesto piano (hay que investigar si esta bien)

--- src/jd/piano/programa/ReproductorMidi.java
+++ src/jd/piano/programa/ReproductorMidi.java
@@ -1,35 +1,76 @@
 package jd.piano.programa;
 
 import jd.piano.teclas.Piano;
+import jd.piano.teclas.Tecla;
 
-import javax.sound.midi.MidiMessage;
-import javax.sound.midi.Receiver;
+import javax.sound.midi.*;
 import java.awt.*;
+import java.io.File;
 
 public class ReproductorMidi implements Receiver{
 
     private static final Color[] COLORES = new Color[16];
-    // private PianoMidi piano; --> PianoMidi no existe
+     private Piano piano; //--> PianoMidi no existe
 
     public ReproductorMidi(){
-        throw new NullPointerException();
+        this.piano = null;
     }
 
-    public void reproducir(String ruta){
-        throw new NullPointerException();
+    public void reproducir(String ruta) throws Exception{
+        File f = new File(ruta);
+        Sequence sequence = MidiSystem.getSequence(f);
+        Sequencer sequencer = MidiSystem.getSequencer();
+        sequencer.open();
+
+        Transmitter transmitter = sequencer.getTransmitter();
+        transmitter.setReceiver(this);
+
+        sequencer.setSequence(sequence);
+        sequencer.start();
+
+        long duracion = sequence.getMicrosecondLength();
+        Thread.sleep(duracion / 1000);
+
+        transmitter.close();
+        sequencer.close();
+
     }
 
     public void conectar(Piano p){
-        throw new NullPointerException();
+        this.piano = piano;
     }
 
     @Override
     public void send(MidiMessage message, long timeStamp) {
-        throw new NullPointerException();
+        if (!(message instanceof ShortMessage)) return;
+
+        ShortMessage sm = (ShortMessage) message;
+        int canal = sm.getChannel();
+        if (canal == 9) return; // canal de percusión
+
+        int nota = sm.getData1();
+        Tecla tecla = piano.getTecla(canal, nota);
+        if (tecla == null) return;
+
+        int comando = sm.getCommand();
+
+        if (comando == ShortMessage.NOTE_ON) {
+            int volumen = sm.getData2();
+            if (volumen > 0) {
+                tecla.setColorPulsado(COLORES[canal]);
+                tecla.pulsar();
+            } else {
+                tecla.soltar();
+            }
+        } else if (comando == ShortMessage.NOTE_OFF) {
+            tecla.soltar();
+        }
+
+        tecla.dibujar();
     }
 
     @Override
     public void close() {
-        throw new NullPointerException();
+
     }
 }
